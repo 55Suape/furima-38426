bb949d4a344d3fe48214d58d48ce58cf993d0cfb
55Sunape <115925957+55Suape@users.noreply.github.com>
2023-02-18 11:08:44 +0900

    ActiveHashの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,15 @@
 class Item < ApplicationRecord
   validates :item_name,presence: true
   validates :spec, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :postage_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :schedule_id, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :postage_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :schedule_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :price, presence: true
 
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
 end
