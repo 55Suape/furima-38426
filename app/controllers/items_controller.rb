f0807c7f30ce540c5a71c1785c476ef7a919be8e
55Sunape <115925957+55Suape@users.noreply.github.com>
2023-02-20 17:24:37 +0900

    商品情報編集機能
    - ルーティングにeditとupdateを追記
    - items_controller.rbにeditとupdateアクションの挙動を追記（showアクションと同じ記述はbefore_actionでまとめる）
    - views/items/show.html.erb編集ボタンのlink_toを修正
    - views/items/edit.html.erbの各種表示項目が出品情報に紐付くよう修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show] # indexとshowアクションの場合を除き、ログインしていないユーザーはログイン画面へ促す
+  before_action :set_item, only: [:edit, :update, :show] # すでに存在しているレコードを選択し中身を書き換えるため、編集したいレコードを@tweetに代入しビューに受け渡す
 
   def index
     @items = Item.all.order("created_at DESC") #item一覧をorderメソッドで降順表示
@@ -19,14 +20,35 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
+
+  #出品ユーザー ≠ カレントユーザー の場合はトップページへ遷移
+  def edit
+    if @item.user == current_user
+       render "edit"
+    else
+      redirect_to root_path
+    end
+  end
+
+  #入力に不備がある場合は編集ページに留まる
+def update
+  if @item.update(item_params)
+    redirect_to item_path
+  else
+    render :edit
+  end
+end
 
   private
 
   def item_params
     params.require(:item).permit(:image, :item_name, :spec, :category_id, :condition_id, :postage_id, :prefecture_id, :schedule_id,
                                  :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
