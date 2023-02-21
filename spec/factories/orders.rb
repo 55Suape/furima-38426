bf153880a9853d8bd8b375712eeee07ed0c6e419
55Sunape <115925957+55Suape@users.noreply.github.com>
2023-02-21 17:02:47 +0900

    単体テスト

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :order do
-    
+    post_code     { '020-0104' }
+    prefecture_id { 2 }
+    city          { '盛岡市' }
+    house_number  { '1-1-1' }
+    phone_number  { '09000000000' }
+
+    association :user,:item
   end
 end
