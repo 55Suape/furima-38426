08e6fdb7c14d249b635e37569488a8a21c4d1dba
55Sunape <115925957+55Suape@users.noreply.github.com>
2023-02-22 18:41:50 +0900

    本番データベースリセット後の対応
    本番のデータベースリセットが完了したため、設定を元に戻す

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,7 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-# bundle exec rake db:migrate
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+bundle exec rake db:migrate
+# 本番のデータベースをリセットする際には、上記bundle exec rake db:migrateをコメントアウト化し、以下の記述を表示させる
+# 本番のデータベースリセットが完了したら、元に戻す
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
