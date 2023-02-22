9b7f089810b24b03d08c902ca255eecc2e66a6f8
55Sunape <115925957+55Suape@users.noreply.github.com>
2023-02-22 16:33:20 +0900

    itemsコントローラー修正（同一条件式をまとめる）
    ## app/controllers/items_controller.rb
    - before_action でuser_checkメソッドを追記し、edit・destroyアクションだけに共通のチェック(出品者とカレントユーザー情報の相違確認)を行う

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show] # indexとshowアクションの場合を除き、ログインしていないユーザーはログイン画面へ促す
-  before_action :set_item, only: [:edit, :update, :show, :destroy] # すでに存在しているレコードを選択し中身を書き換えるため、編集したいレコードを@tweetに代入しビューに受け渡す
+  before_action :set_item, only: [:edit, :update, :show, :destroy] # すでに存在しているレコードを選択し中身を書き換えるため、編集したいレコードを@itemに代入しビューに受け渡す
+  before_action :user_check, only: [:edit, :destroy] # 出品ユーザー ≠ カレントユーザーの場合はトップページへ遷移
 
   def index
     @items = Item.all.order('created_at DESC') # item一覧をorderメソッドで降順表示
@@ -24,7 +25,7 @@ class ItemsController < ApplicationController
 
   # 出品ユーザー = カレントユーザー且つSold Outになっていない場合は編集画面へ遷移、それ以外はトップページへ遷移
   def edit
-    if @item.user == current_user && @item.order.nil?
+    if @item.order.nil?
       render 'edit'
     else
       redirect_to root_path
@@ -40,14 +41,10 @@ class ItemsController < ApplicationController
     end
   end
 
-  # 出品ユーザー ≠ カレントユーザー の場合はトップページへ遷移、イコールの場合は削除してトップページへ遷移
+  # 削除してトップページへ遷移
   def destroy
-    if @item.user == current_user
-      @item.destroy
-      redirect_to root_path
-    else
-      redirect_to root_path
-    end
+    @item.destroy
+    redirect_to root_path
   end
 
   private
@@ -60,4 +57,10 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
+
+  def user_check
+    @user = @item.user
+    redirect_to(root_path) unless @user == current_user
+  end
+
 end
